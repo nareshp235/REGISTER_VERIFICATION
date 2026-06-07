259c41258e0ec3a160442e9ea0569f8ae3069e09
nareshp235 <naresh.p235@gmail.com>
2026-06-07 11:46:11 +0530

    Create reg_defines.svh

---
+++ include/reg_defines.svh
@@ -0,0 +1,13 @@
+//==============================================================================
+// File      : reg_defines.svh
+// Description:
+//   Common Register Definitions
+//==============================================================================
+
+`ifndef REG_DEFINES_SVH
+`define REG_DEFINES_SVH
+
+`define CTRL_REG_ADDR 32'h0000_1000
+
+`endif
+
